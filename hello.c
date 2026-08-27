7443257175c326ef1ec83540651275f0760316fa
PaxtonL30 <33264834@nebraska.edu>
2026-08-27 10:27:02 -0500

    initial commit

---
+++ hello.c
@@ -0,0 +1,16 @@
+/**
+ * Author: Paxton Lamprecht
+ * Date: 2026/8/25
+ *
+ * A simple hello world program in C
+ *
+ */
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+
+  printf("My name is Paxton\n");
+
+  return 0;
+}
